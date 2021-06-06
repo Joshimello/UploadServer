e2dfc4b3dd899bf22491836a12620775bd06d384
Joshimello <74640729+Joshimello@users.noreply.github.com>
2021-06-06 10:14:07 +0800

    Add files via upload

---
+++ post/example.php
@@ -0,0 +1,15 @@
+
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>just an example</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu" />
+        </head>
+        <body style="font-family: ubuntu;">
+            <span>author:<b> someone</b></span><br>
+            <p style="overflow-wrap: break-word; white-space: pre-wrap;">some text i guess</p>
+            <img href="uploads/neko.jpg"></img>
+        </body>
+        </html>
+        
